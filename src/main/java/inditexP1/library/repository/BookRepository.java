f57598e5e1030de8cbf486a9e13730dbd2b66041
Juan Luis Márquez <juan.luis.marquezcan@gmail.com>
2026-04-09 10:39:35 +0200

    fix: erased 2 white lines over repository anotation.

--- src/main/java/inditexP1/library/repository/BookRepository.java
+++ src/main/java/inditexP1/library/repository/BookRepository.java
@@ -4,8 +4,6 @@ import inditexP1.library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
 }
