0f80bd5e05eb23de4f7f0df6b5b9f42e451fcd6b
Juan Luis Márquez <juan.luis.marquezcan@gmail.com>
2026-04-09 14:39:29 +0200

    feat: service updated with author crud

--- src/main/java/inditexP1/library/services/AuthorService.java
+++ src/main/java/inditexP1/library/services/AuthorService.java
@@ -4,7 +4,12 @@ package inditexP1.library.services;
 import inditexP1.library.model.Author;
 import inditexP1.library.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,13 +20,13 @@ public class AuthorService {
     private AuthorRepository authorRepository;
 
     //GET todos
-    public List<Author> getAllAuthors(){
+    public List<Author> getAllAuthors() {
         return authorRepository.findAll();
     }
 
     //GET por ID
 
-    public Optional<Author> getAuthorById(int id){
+    public Optional<Author> getAuthorById(int id) {
         return authorRepository.findById(id);
     }
 
@@ -30,6 +35,22 @@ public class AuthorService {
     public void deleteAuthor(int id) {
         authorRepository.deleteById(id);
     }
+        boolean editAuthor( int id, Author author){
+            try {
+                if (this.getAuthorById(id).isEmpty()) {
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Author with that id not found in our DB")
+                    Author authorEdit = this.getAuthorById(id).get();
+                    authorEdit.setName(author.getName());
+                    authorEdit.setNationality(author.getNationality());
+                    authorEdit.setAlive(author.getAlive());
+                    authorEdit.setSurname(author.getSurname());
+                    authorEdit.setBirthTear(author.getBirthTear());
+                }
+            } catch (Exception e) {
+                System.out.println("Ha ocurrido un error: \n " + e.getMessage());
+            }
+            return true;
+        }
 
 
 }
