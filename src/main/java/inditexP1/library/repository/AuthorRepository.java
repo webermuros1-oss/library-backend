f9d569d6d47ff93a9bc7d5dfc5818b1c76dc1d0e
Juan Luis Márquez <juan.luis.marquezcan@gmail.com>
2026-04-09 12:09:05 +0200

    methods to add books and authirs done(repository+service)

--- src/main/java/inditexP1/library/repository/AuthorRepository.java
+++ src/main/java/inditexP1/library/repository/AuthorRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
     Optional<Author> findById(int searchedId);
+
 }
--- src/main/java/inditexP1/library/services/LibraryService.java
+++ src/main/java/inditexP1/library/services/LibraryService.java
@@ -12,6 +12,20 @@ import java.util.Optional;
 public class LibraryService{
     AuthorRepository authorRepository;
     BookRepository bookRepository;
+ //put
 
+  //post
+   public boolean addAuthor(Author author){
+       int searchedId = author.getId();
+       if(authorRepository.findById(searchedId).isPresent()) return false;
+       authorRepository.save(author);
+       return true;
+   }
 
+    public boolean addBook(Book book){
+        int searchedId = book.getId();
+        if(bookRepository.findById(searchedId).isPresent()) return false;
+        bookRepository.save(book);
+        return true;
+    }
 }
